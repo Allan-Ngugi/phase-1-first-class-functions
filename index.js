4504da920aa0a767089c30cfbba6351e3db066aa
Allan-Ngugi <allan.ngugi@student.moringaschool.com>
2022-08-20 15:54:20 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction (thing) {
+    return thing();
+}
+function returnsANamedFunction () {
+    return function HelloWorld (){return 'Hello' + 'World'}
+}
+function returnsAnAnonymousFunction (){
+    return function() {return 'Hello'}
+}
